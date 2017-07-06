d0b7760148697904d2b59c785fc46bf2b36c8279
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2017-07-05 21:17:45 -0500

    Configuracion inicial del proyecto

---
+++ app.js
@@ -0,0 +1,4 @@
+const got = require('got');
+const nodemailer = require('nodemailer');
+const gmail = require("./credenciales/gmail.js");
+
---
+++ credenciales/gmail.sample.js
@@ -0,0 +1,20 @@
+// Credenciales para el API de Gmail
+
+module.exports.auth = {
+    /**
+     * User
+     */
+    user: 'usuario@gmail.com',
+    /**
+     * ID de cliente
+     */
+    clientId: 'RANDOM-123agf234jouugou87cre234q3.apps.googleusercontent.com',
+    /**
+     * Secreto de cliente
+     */
+    clientSecret: 'RANDOM-E42354Y5F-w83245gm',
+    /**
+     * Refresh Token
+     */
+    refreshToken: 'RANDOM-m345QUUsdfV_2354z7adLlvMM234HKgdv5GKw'
+}
