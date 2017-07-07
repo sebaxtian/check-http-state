e0ec9592d257cb4dc4004cc6f9ee304310bb7424
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2017-07-07 15:19:20 -0500

    Agrego configuracion para ejecutar desde Crontab Linux

---
+++ crontab/check-http-state.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Script para ejecutar aplicacion Node check-http-state
+#
+
+# 1. Carga el PATH donde se encuentra check-http-state
+cd /path/to/node/check-http-state/
+# 2. Ejecuta la aplicacion de Node check-http-state
+node app.js
